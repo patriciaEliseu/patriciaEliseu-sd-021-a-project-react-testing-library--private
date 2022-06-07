503f82e92aac2c43c13bc50f8468cb51042cad31
Patricia Eliseu <patyeliseu4@gmail.com>
2022-06-07 18:34:51 -0300

    add requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('testar se existe um link Home na pagina inicial', () => {});
+/* eslint-disable func-call-spacing */
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../pages/About';
+
+describe('o About Será avaliado 100% pelo Stryker.', () => {
+  test('testar se a pagina contém um heding h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const headingH2 = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(headingH2).toBeInTheDocument();
+  });
+  test('testar se a pagina contem dois paragrafos', () => {
+    renderWithRouter(<About />);
+    const paragrafo1 = screen.getByText(/this application simulates a pokédex, /i);
+    expect(paragrafo1).toBeInTheDocument();
+    const paragrafo2 = screen.getByText(/one can filter pokémons by type, /i);
+    expect(paragrafo2).toBeInTheDocument();
+  });
+  test('testar se a pagina contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const imagemP = screen.getByRole('img', { name: /pokédex/i });
+    // expect(imagemP).to('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(imagemP).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
